58ad7cf6286849913eb62151ead3fd0ad4b45f0b
Mustafa Barodawala <24886321+itsjustmustafa@users.noreply.github.com>
2025-04-04 18:20:19 +1100

    birdvote code

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,151 @@
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-import reactLogo from "./assets/image_10.jpg";
-import viteLogo from "./assets/image_13.jpg";
-import "./App.css";
+import { useRef, useState } from "react";
+import styles from "./App.module.css";
+import birdImages from "./birdimages";
+import birdLogo from "./assets/birdvote.png";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [votingResults, setVotingResults] = useState<number[]>([]);
+    const resultAreaRef = useRef<HTMLDivElement>(null);
+
+    const [voteSubmitted, setVoteSubmitted] = useState<boolean>(false);
+
+    const progress_message = `${Math.floor(
+        (100 * votingResults.length) / birdImages.length
+    )}% Complete`;
+
+    function addVote(index: number) {
+        if (!votingResults.includes(index)) {
+            setVotingResults([...votingResults, index]);
+            setVoteSubmitted(false);
+        }
+        if (resultAreaRef.current) {
+            resultAreaRef.current.scrollLeft =
+                resultAreaRef.current.scrollWidth;
+        }
+    }
+
+    function removeVote(index: number) {
+        setVotingResults((previousVotingResults) => {
+            const newVotingResults = [...previousVotingResults];
+            newVotingResults.splice(index, 1);
+            return newVotingResults;
+        });
+    }
+
+    function moveLeft(index: number) {
+        setVotingResults((prevVotingResults) =>
+            swapAtIndex(prevVotingResults, index, -1)
+        );
+        setVoteSubmitted(false);
+    }
+
+    function moveRight(index: number) {
+        setVotingResults((prevVotingResults) =>
+            swapAtIndex(prevVotingResults, index, 1)
+        );
+        setVoteSubmitted(false);
+    }
+
+    function swapAtIndex<T>(arr: T[], index: number, offset: number) {
+        // Swap with offseted index
+        const otherIndex = index + offset;
+
+        if (otherIndex < 0 || otherIndex >= arr.length) {
+            return arr;
+        }
+
+        const selectedValue = arr[index];
+        const otherValue = arr[otherIndex];
+
+        const newArr = [...arr];
+        newArr[index] = otherValue;
+        newArr[otherIndex] = selectedValue;
+        return newArr;
+    }
+
+    function submitVote() {
+        setVoteSubmitted(true);
+        const ballot = votingResults
+            .map((index) => (index + 1).toString())
+            .join(" ");
+        const ballotText = `||BALLOT: ${ballot}||`;
+        // console.log("yo mama");
+        navigator.clipboard.writeText(ballotText);
+    }
 
     return (
         <>
             <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
+                <span>
+                    <h1 className={styles.header}>
+                        RANKED CHOICE BIRD VOTING!!!
+                    </h1>
+                    <span>
+                        <img src={birdLogo} className={styles.pageLogo} />
+                    </span>
+                </span>
+            </div>
+            <div className={styles.voting_area}>
+                {birdImages.map((birdImage, index) => (
                     <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
+                        src={birdImage}
+                        key={index}
+                        draggable={false}
+                        onClick={() => addVote(index)}
+                        className={
+                            `${
+                                votingResults.includes(index)
+                                    ? styles.selected
+                                    : styles.unselected
+                            } ` + styles.bird_image
+                        }
                     />
-                </a>
+                ))}
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+            <div>Your Vote ({progress_message}):</div>
+            <div className={styles.result_area} ref={resultAreaRef}>
+                {votingResults.map((imageIndex, index) => (
+                    <div className={styles.result} key={index}>
+                        <button
+                            className={styles.remove_button}
+                            onClick={() => removeVote(index)}
+                        >
+                            X
+                        </button>
+                        <img
+                            src={birdImages[imageIndex]}
+                            className={styles.bird_image}
+                            draggable={false}
+                        />
+                        <div className={styles.rank}>
+                            <button
+                                className={styles.move_button}
+                                onClick={() => moveLeft(index)}
+                            >
+                                <span className={styles.horizontal}>⬅️</span>
+                                <span className={styles.vertical}>⬆️</span>
+                            </button>
+                            <p>#{index + 1}</p>
+                            <button
+                                className={styles.move_button}
+                                onClick={() => moveRight(index)}
+                            >
+                                <span className={styles.horizontal}>➡️</span>
+                                <span className={styles.vertical}>⬇️</span>
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <button
+                onClick={() => submitVote()}
+                className={styles.submit_button}
+            >
+                SUBMIT MY VOTE
+            </button>
+            {voteSubmitted && (
+                <p>Vote copied to clipboard, please send on discord!</p>
+            )}
         </>
     );
 }
---
+++ src/birdimages.ts
@@ -0,0 +1,45 @@
+import image1 from "./assets/image_1.jpg";
+import image2 from "./assets/image_2.jpg";
+import image3 from "./assets/image_3.jpg";
+import image4 from "./assets/image_4.jpg";
+import image5 from "./assets/image_5.jpg";
+import image6 from "./assets/image_6.jpg";
+import image7 from "./assets/image_7.jpg";
+import image8 from "./assets/image_8.jpg";
+import image9 from "./assets/image_9.jpg";
+import image10 from "./assets/image_10.jpg";
+import image11 from "./assets/image_11.jpg";
+import image12 from "./assets/image_12.jpg";
+import image13 from "./assets/image_13.jpg";
+import image14 from "./assets/image_14.jpg";
+import image15 from "./assets/image_15.jpg";
+import image16 from "./assets/image_16.jpg";
+import image17 from "./assets/image_17.jpg";
+import image18 from "./assets/image_18.jpg";
+import image19 from "./assets/image_19.jpg";
+import image20 from "./assets/image_20.jpg";
+
+const birdImages = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+    image20,
+];
+
+export default birdImages;
